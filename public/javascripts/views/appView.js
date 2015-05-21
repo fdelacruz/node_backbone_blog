25cb15ec93c5acf3d86d74ae71f157bcbf0ad9f2
Francisco De La Cruz <helloquico@gmail.com>
2015-05-21 17:08:08 -0400

    Add New Posts

--- public/javascripts/views/appView.js
+++ public/javascripts/views/appView.js
@@ -2,6 +2,9 @@ var app = app || {};
 
 app.AppView = Backbone.View.extend({
 	el: '#blog',
+	events: {
+		'submit #newpost': 'newPostSubmit'
+	},
 	initialize: function() {
 		this.listenTo(app.posts, 'add', this.addOne);
 	},
@@ -12,5 +15,15 @@ app.AppView = Backbone.View.extend({
 		this.$('#posts').append(
 			view.render().el
 		);
+	},
+	newPostSubmit: function(e) {
+		e.preventDefault();
+		var attributes = {
+			title: this.$('[name="title"]').val(),
+			content: this.$('[name="content"]').val(),
+			posted: new Date()
+		};
+		app.posts.create(attributes);
+		this.$('[type="text"]').val('');
 	}
 });
